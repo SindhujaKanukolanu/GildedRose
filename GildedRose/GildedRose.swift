8c1979fb1806858bafd7da0825c186fd4c634ecc
Kanukolanu Sri Sai Sindhuja <kanukolanu.sri.sai.sindhuja@sap.com>
2018-04-26 11:07:20 +0530

    Changes

--- GildedRose/GildedRose.swift
+++ GildedRose/GildedRose.swift
@@ -8,82 +8,94 @@
 
 public class GildedRose {
 
-	public static func updateItem(items: [Item]) -> [Item] {
-		var items = items
-		for index in 0 ..< items.count {
-			items[index] = updateSellIn(item: items[index])
-			items[index] = updateQuality(item: items[index])
-		}
-		return items
-	}
+    public static func updateItem(items: [Item]) -> [Item] {
+        var items = items
+        for index in 0 ..< items.count {
+            items[index] = updateSellIn(for: items[index])
+            items[index] = updateQuality(for: items[index])
+        }
+        return items
+    }
 
-	private static func updateSellIn(item: Item) -> Item {
-		var updatedItem: Item = item
-		if updatedItem.name != ItemName.sulfuras.rawValue {
-			updatedItem.sellIn -= 1
-		}
-		return updatedItem
-	}
+    private static func updateSellIn(for item: Item) -> Item {
+        var updatedItem: Item = item
+        if updatedItem.name != ItemName.sulfuras.rawValue {
+            updatedItem.sellIn -= 1
+        }
+        return updatedItem
+    }
 
-	private static func updateQuality(item: Item) -> Item {
-		var updatedItem = item
-		switch updatedItem.name {
-		case ItemName.agedBrie.rawValue:
-			if updatedItem.quality < 50 {
-				updatedItem.quality += 1
-			}
-		case ItemName.backStagePasses.rawValue:
-			updatedItem = updateBackStagePassesQuality(item: updatedItem)
-		case ItemName.sulfuras.rawValue:
-			updatedItem.quality += 0
-		case ItemName.conjured.rawValue:
-			updatedItem.quality -= 2
-			if updatedItem.quality < 0 {
-				updatedItem.quality = 0
-			}
-		default:
-			updatedItem = updateNormalItem(item: updatedItem)
-		}
-		return updatedItem
-	}
+    private static func updateQuality(for item: Item) -> Item {
+        var updatedItem = item
+        switch updatedItem.name {
+        case ItemName.agedBrie.rawValue:
+            updatedItem = updateAgedBrieQuality(item: updatedItem)
+        case ItemName.backStagePasses.rawValue:
+            updatedItem = updateBackStagePassesQuality(item: updatedItem)
+        case ItemName.sulfuras.rawValue:
+            updatedItem.quality += 0
+        case ItemName.conjured.rawValue:
+            updatedItem = updateConjuredQuality(item: updatedItem)
+        default:
+            updatedItem = updateNormalItem(item: updatedItem)
+        }
+        return updatedItem
+    }
 
-	private static func updateBackStagePassesQuality(item: Item) -> Item {
-		var updatedItem = item
-		updatedItem.quality += 1
-		if isSellin_10DaysOrLess(item: updatedItem) {
-			updatedItem.quality += 1
-		}
-		if isSellin_5DaysOrLess(item: updatedItem) {
-			updatedItem.quality += 2
-		}
-		if updatedItem.sellIn < 0 {
-			updatedItem.quality = 0
-		}
-		if updatedItem.quality > 50 {
-			updatedItem.quality = 50
-		}
-		return updatedItem
-	}
+    private static func updateAgedBrieQuality(item: Item) -> Item {
+        var updatedItem = item
+        if updatedItem.quality < 50 {
+            updatedItem.quality += 1
+        }
+        return updatedItem
+    }
 
-	private static func isSellin_10DaysOrLess(item: Item) -> Bool {
-		return item.sellIn <= 10 && item.sellIn > 5
-	}
+    private static func updateConjuredQuality(item: Item) -> Item {
+        var updatedItem = item
+        updatedItem.quality -= 2
+        if updatedItem.quality < 0 {
+            updatedItem.quality = 0
+        }
+        return updatedItem
+    }
 
-	private static func isSellin_5DaysOrLess(item: Item) -> Bool {
-		return item.sellIn <= 5 && item.sellIn >= 0
-	}
+    private static func updateBackStagePassesQuality(item: Item) -> Item {
+        var updatedItem = item
+        updatedItem.quality += 1
+        if isSellin10daysOrLess(item: updatedItem) {
+            updatedItem.quality += 1
+        }
+        if isSellin5daysOrLess(item: updatedItem) {
+            updatedItem.quality += 2
+        }
+        if updatedItem.sellIn < 0 {
+            updatedItem.quality = 0
+        }
+        if updatedItem.quality > 50 {
+            updatedItem.quality = 50
+        }
+        return updatedItem
+    }
 
-	private static func updateNormalItem(item: Item) -> Item {
-		var updatedItem = item
-		if updatedItem.sellIn < 0 {
-			updatedItem.quality -= 2
-		} else {
-			updatedItem.quality -= 1
-		}
-		if updatedItem.quality < 0 {
-			updatedItem.quality = 0
-		}
-		return updatedItem
-	}
+    private static func isSellin10daysOrLess(item: Item) -> Bool {
+        return item.sellIn <= 10 && item.sellIn > 5
+    }
+
+    private static func isSellin5daysOrLess(item: Item) -> Bool {
+        return item.sellIn <= 5 && item.sellIn >= 0
+    }
+
+    private static func updateNormalItem(item: Item) -> Item {
+        var updatedItem = item
+        if updatedItem.sellIn < 0 {
+            updatedItem.quality -= 2
+        } else {
+            updatedItem.quality -= 1
+        }
+        if updatedItem.quality < 0 {
+            updatedItem.quality = 0
+        }
+        return updatedItem
+    }
 
 }
